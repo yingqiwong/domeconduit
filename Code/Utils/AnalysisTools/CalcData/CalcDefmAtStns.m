3016da1b36bc5b574d931d63105e0a0ac43b6a0f
Ying Qi Wong <yqwong@sr14-d01451f1cb.stanford.edu>
2019-11-19 07:52:28 -0800

    some edits before running initial nbr tests

--- Code/Utils/AnalysisTools/CalcData/CalcDefmAtStns.m
+++ Code/Utils/AnalysisTools/CalcData/CalcDefmAtStns.m
@@ -1,4 +1,4 @@
-function [u] = CalcDefmAtStns (td, m, time, StnPosXYZ, StnInd)
+function [uOut] = CalcDefmAtStns (td, m, time, PosXYZ, StnInd)
 
 % material parameters
 mu      = m.ch.mu;              % shear modulus
@@ -16,14 +16,13 @@ dpch_td = m.slv.sy(1)*(td.y(1,:) - td.y(1,1));
 tyr     = ConvertSecToYear(td.x);
 dpch    = interp1(tyr, dpch_td, time, 'pchip');
 
-
 u = zeros(length(time), 3);
 for ti = 1:length(time)
     params(4) = dpch(ti);
     [u(ti,1), u(ti,2), u(ti,3)] = ...
-        fcn_yangM(params, StnPosXYZ(StnInd(ti),1), StnPosXYZ(StnInd(ti),2), matrl, 0);
+        fcn_yangM(params, PosXYZ(StnInd(ti),1), PosXYZ(StnInd(ti),2), matrl, 0);
 end
 
-
+uOut = 1e3*u; % convert to mm
 
 end
